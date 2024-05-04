f075a4ca648494f528166552052acb93f0344533
vikrant-verma9 <aniketverma020@gmail.com>
2024-05-04 12:33:50 +0530

    message

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const dishes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = 3011;
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://VikrantVerma:9044607960@cluster0.yag7mys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
+
+app.use("/api", dishes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ backend/models/Dishes.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DishesModel = mongoose.model("fooditems", {
+  name: { type: String, required: true },
+  img: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+module.exports = DishesModel;
