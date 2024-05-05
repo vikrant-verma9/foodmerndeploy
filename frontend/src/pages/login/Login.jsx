7467e3b1a61c7ec2b391e4de8fccc7cfeb746f37
vikrant-verma9 <aniketverma020@gmail.com>
2024-05-05 11:27:24 +0530

    message

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -31,7 +31,8 @@ const Login = () => {
   const registerUser = async (currentUser) => {
     try {
       const response = await axios.post(
-        `http://localhost:3001/api/user/register`,
+        `https://foodmerndeploy.vercel.app/api/user/register`,
+      
         currentUser
       );
       const { user, token } = response.data;
@@ -49,7 +50,7 @@ const Login = () => {
   const loginUser = async (currentUser) => {
     try {
       const response = await axios.post(
-        `http://localhost:3000/api/user/login`,
+        `https://foodmerndeploy.vercel.app/api/user/login`,
         currentUser
       );
       const { user, token } = response.data;
--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -15,7 +15,7 @@ const Shop = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`http://localhost:3001/api/dishes`);
+        const response = await axios.get(`https://foodmerndeploy.vercel.app/api/dishes`);
         console.log(response.data);
         setProduct(response.data);
         setFoodOnly(response.data.filter((item) => item.category === "food"));
